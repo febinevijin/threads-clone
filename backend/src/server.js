36c56a0fb663ba293d34e84e751e0d2784897503
Febin Evijin <15febinevijin@gmail.com>
2024-07-15 19:22:08 +0530

    setup for deploy

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
-// import path from 'path';
+import path from 'path';
+import fs from 'fs';
 // import { fileURLToPath } from 'url';
 // const __filename = fileURLToPath(import.meta.url);
 // const __dirname = path.dirname(__filename);
@@ -25,9 +26,10 @@ import authRouter from './router/authRouter.js';
 import userRouter from './router/userRouter.js';
 import postRouter from './router/postRouter.js';
 import messageRouter from './router/messageRouter.js';
-import {app,server} from './socket/socket.js'
+import { app, server } from './socket/socket.js';
 
 connectDB();
+const __dirname = path.resolve();
 // const app = express(); no need this , already initialized server.js
 const whitelist = appConfig.whiteList.split(',');
 
@@ -83,8 +85,53 @@ app.use('/api/messages', messageRouter);
 app.use(notFound);
 app.use(errorHandler);
 
+
+
+// http://localhost:5000 => backend,frontend
+
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+//   // react app
+
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+//   });
+// }
+
+console.log('NODE_ENV:', process.env.NODE_ENV);
+if (process.env.NODE_ENV === 'development') {
+  const staticFilesPath = path.join(__dirname, '/frontend/dist');
+  console.log('Static files directory:', staticFilesPath);
+
+  app.use(express.static(staticFilesPath));
+
+  // Catch-all route to serve the index.html file
+  app.get('*', async(req, res) => {
+    console.log('Serving the index.html file for route:', req.originalUrl);
+    const indexPath = await path.resolve(__dirname, 'frontend', 'dist', 'index.html');
+    console.log('Index file path:', indexPath);
+    res.sendFile(indexPath, (err) => {
+      if (err) {
+        console.error('Error serving index.html:', err);
+        res.status(500).send(err);
+      }
+    });
+  });
+}
+
+
+const distPath = path.join('./frontend/dist');
+fs.readdir(distPath, (err, files) => {
+  if (err) {
+    console.log('Error reading dist directory', err);
+  } else {
+    console.log('Dist directory contents:', files);
+  }
+});
+
 const PORT = appConfig.port;
 
- server.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
